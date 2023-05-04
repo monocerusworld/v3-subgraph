56714b1b11f34df9c22ae9d8c2d574bbdc1a7944
pullstacker <admin@tartz.one>
2023-05-04 15:30:07 +0700

    tartz fantom

--- src/utils/pricing.ts
+++ src/utils/pricing.ts
@@ -4,42 +4,65 @@ import { Bundle, Pool, Token } from './../types/schema'
 import { BigDecimal, BigInt } from '@graphprotocol/graph-ts'
 import { exponentToBigDecimal, safeDiv } from '../utils/index'
 
-const WETH_ADDRESS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
-const USDC_WETH_03_POOL = '0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8'
+
+//MOONBEAM
+// const WETH_ADDRESS = '0xab3f0245B83feB11d15AAffeFD7AD465a59817eD'
+// const USDC_WETH_03_POOL = '0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8' //tbd
+
+// // token where amounts should contribute to tracked volume and liquidity
+// // usually tokens that many tokens are paired with s
+// export let WHITELIST_TOKENS: string[] = [
+//   WETH_ADDRESS, // WETH
+//   '0x765277EebeCA2e31912C9946eAe1021199B39C61', // DAI
+//   '0x818ec0A7Fe18Ff94269904fCED6AE3DaE6d6dC0b', // USDC
+//   '0xeFAeeE334F0Fd1712f9a8cc375f427D9Cdd40d73', // USDT
+//   '0x922D641a426DcFFaeF11680e5358F34d97d112E1', // WBTC
+// ]
+
+// let STABLE_COINS: string[] = [
+//   '0x765277EebeCA2e31912C9946eAe1021199B39C61', // DAI
+//   '0x818ec0A7Fe18Ff94269904fCED6AE3DaE6d6dC0b', // USDC
+//   '0xeFAeeE334F0Fd1712f9a8cc375f427D9Cdd40d73', // USDT
+// ]
+
+//GNOSIS
+// const WETH_ADDRESS = '0x6A023CCd1ff6F2045C3309768eAd9E68F978f6e1'
+// const USDC_WETH_03_POOL = '0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8' //tbd
+
+// // token where amounts should contribute to tracked volume and liquidity
+// // usually tokens that many tokens are paired with s
+// export let WHITELIST_TOKENS: string[] = [
+//   WETH_ADDRESS, // WETH
+//   '0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d', // DAI
+//   '0xDDAfbb505ad214D7b80b1f830fcCc89B60fb7A83', // USDC
+//   '0x4ECaBa5870353805a9F068101A40E0f32ed605C6', // USDT
+//   '0x8e5bBbb09Ed1ebdE8674Cda39A0c169401db4252', // WBTC
+// ]
+
+// let STABLE_COINS: string[] = [
+//   '0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d', // DAI
+//   '0xDDAfbb505ad214D7b80b1f830fcCc89B60fb7A83', // USDC
+//   '0x4ECaBa5870353805a9F068101A40E0f32ed605C6', // USDT
+// ]
+
+//FANTOM
+const WETH_ADDRESS = '0xab3f0245B83feB11d15AAffeFD7AD465a59817eD'
+const USDC_WETH_03_POOL = '0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8' //tbd
 
 // token where amounts should contribute to tracked volume and liquidity
 // usually tokens that many tokens are paired with s
 export let WHITELIST_TOKENS: string[] = [
   WETH_ADDRESS, // WETH
-  '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI
-  '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USDC
-  '0xdac17f958d2ee523a2206206994597c13d831ec7', // USDT
-  '0x0000000000085d4780b73119b644ae5ecd22b376', // TUSD
-  '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', // WBTC
-  '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643', // cDAI
-  '0x39aa39c021dfbae8fac545936693ac917d5e7563', // cUSDC
-  '0x86fadb80d8d2cff3c3680819e4da99c10232ba0f', // EBASE
-  '0x57ab1ec28d129707052df4df418d58a2d46d5f51', // sUSD
-  '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2', // MKR
-  '0xc00e94cb662c3520282e6f5717214004a7f26888', // COMP
-  '0x514910771af9ca656af840dff83e8264ecf986ca', // LINK
-  '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f', // SNX
-  '0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e', // YFI
-  '0x111111111117dc0aa78b770fa6a738034120c302', // 1INCH
-  '0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8', // yCurv
-  '0x956f47f50a910163d8bf957cf5846d573e7f87ca', // FEI
-  '0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0', // MATIC
-  '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9', // AAVE
-  '0xfe2e637202056d30016725477c5da089ab0a043a' // sETH2
+  '0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E', // DAI
+  '0x04068DA6C83AFCFA0e13ba15A6696662335D5B75', // USDC
+  '0x049d68029688eAbF473097a2fC38ef61633A3C7A', // USDT
+  '0x321162Cd933E2Be498Cd2267a90534A804051b11', // WBTC
 ]
 
 let STABLE_COINS: string[] = [
-  '0x6b175474e89094c44da98b954eedeac495271d0f',
-  '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-  '0xdac17f958d2ee523a2206206994597c13d831ec7',
-  '0x0000000000085d4780b73119b644ae5ecd22b376',
-  '0x956f47f50a910163d8bf957cf5846d573e7f87ca',
-  '0x4dd28568d05f09b02220b09c2cb307bfd837cb95'
+  '0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E', // DAI
+  '0x04068DA6C83AFCFA0e13ba15A6696662335D5B75', // USDC
+  '0x049d68029688eAbF473097a2fC38ef61633A3C7A', // USDT
 ]
 
 let MINIMUM_ETH_LOCKED = BigDecimal.fromString('60')
